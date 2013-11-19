e170f03ed04dc1d4ed9058562f392218102a46c3
mefuru <mehul.mandania@gmail.com>
2013-11-19 15:35:43 -0500

    stream.js

--- stream.js
+++ stream.js
@@ -1,6 +1,12 @@
 var util = require('util'),
 twitter = require('twitter'),
 keys = require('./keys2.js');
+console.log('You are comparing %s and %s', process.argv[2], process.argv[3]);
+var input1 = process.argv[2];
+var input2 = process.argv[3];
+var counter1 = 0;
+var counter2 = 0;
+
 var twit = new twitter({
     consumer_key: keys.consumer_key,
     consumer_secret: keys.consumer_secret,
@@ -8,12 +14,20 @@ var twit = new twitter({
     access_token_secret: keys.access_token_secret
 });
 
-twit.stream('filter', {track:'NYC'}, function(stream) {
-    stream.on('data', function(data) {
-        console.log(util.inspect(data));
+var switchARooney = function(input) {
+    twit.stream('filter', {track:input}, function(stream) {
+        stream.on('data', function(data) {
+            input==input1 ? counter1++ : counter2++;
+        });
+        setTimeout(moveOn, 500, stream, input);
     });
-    // Disconnect stream after five seconds
-    setTimeout(stream.destroy, 10000);
-});
+};
 
+var moveOn = function(stream, input) {
+    stream.destroy;
+    input==input1 ? input=input2 :input=input1;
+    console.log(counter1, counter2);
+    switchARooney(input);
+};
 
+switchARooney(input1);
